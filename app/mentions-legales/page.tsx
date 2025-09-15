b8b094d7c69453980fd26dd62d6676e604d2436c
Matthieu Compérat <matthieu.comperat@gmail.com>
2025-09-15 18:04:36 +0200

    feat(content): Homepage & Mentions

---
+++ app/mentions-legales/page.tsx
@@ -0,0 +1,57 @@
+export default function Mentions() {
+  return (
+    <main className="py-8 text-zinc-900">
+      <article className="container mx-auto">
+        <h3 className="mb-4 text-xl font-bold text-sky-700">Mentions légales</h3>
+        <h4 className="font-noto mb-2 text-lg font-bold text-gray-800 uppercase">
+          Droits d&apos;auteurs et copyright :
+        </h4>
+        <p>
+          Le site de l&apos;association &quot;Chœur des Pays du Mont-Blanc&quot; est protégé par la législation
+          française et internationale sur le droit d&apos;auteur et la propriété intellectuelle. Les droits de
+          l&apos;auteur de ce site sont réservés pour toute forme d&apos;utilisation. En particulier, la reproduction
+          des éléments graphiques du site, le téléchargement complet du site pour son enregistrement sur un support de
+          diffusion, ainsi que toute utilisation des visuels et textes qu&apos;il contient autre que la consultation
+          individuelle et privée sont interdits sauf autorisation expresse du directeur de la publication.
+        </p>
+        <h4 className="font-noto my-2 text-lg font-bold text-gray-800 uppercase">Données personnelles :</h4>
+        <h5 className="font-bold">1. Déclaration à la CNIL :</h5>
+        <p>
+          Conformément à l&apos;article n° 2010-229 du 10 juin 2010 dispensant de déclaration les traitements
+          automatisés de données à caractère personnel mis en œuvre par des organismes à but non lucratif abrogeant et
+          remplaçant la délibération n° 2006-130 du 9 mai 2006, il n&apos;y a pas de déclaration de fichier à la CNIL.
+        </p>
+        <h5 className="mt-1 font-bold">2. Collecte de données personnelles :</h5>
+        <p>
+          Les données personnelles sont des informations qui, par un moyen direct ou indirect, permettent de désigner
+          momentanément un utilisateur et de le rattacher éventuellement aux données collectées.
+        </p>
+        <p>
+          Le site de l&apos;association ne collecte pas d&apos;informations personnelles permettant de vous identifier.
+          A l&apos;exception des membres de l&apos;association, les données collectées sont uniquement l&apos;adresse de
+          votre courriel. Cette dernière n&apos;est pas cédée ou revendue.
+        </p>
+        <p>
+          Conformément à la loi &quot;Informatique et Libertés&quot; du 6 janvier 1978 modifiée en 2004, vous disposez
+          d&apos;un droit d&apos;accès, de rectification et de suppression des données vous concernant. Pour exercer ce
+          droit, vous pouvez nous contacter à l&apos;adresse suivante :&nbsp;
+          <a href="mailto:bureau@choeurdespaysdumontblanc.fr" className="text-sky-700 hover:underline">
+            bureau@choeurdespaysdumontblanc.fr
+          </a>
+          .
+        </p>
+        <h5 className="mt-1 font-bold">3. Publicité électronique :</h5>
+        <p>
+          L&apos;envoi de courrier électronique à des fins de publicité suppose que vous ayez exprimé votre accord
+          préalable. Vous pouvez vous opposer à l&apos;utilisation de ces coordonnées par courrier envoyé à
+          l&apos;adresse de l&apos;association ou par désincription lors de la réception d&apos;un courriel de type
+          &quot;Lettre d&apos;information&quot;.
+        </p>
+      </article>
+      <p className="container mx-auto mt-4">
+        Licences entrepreneur du spectacle : <span className="font-light">PLATESV-D-2022-005692</span> et{" "}
+        <span className="font-light">PLATESV-D-2022-005721</span>.
+      </p>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,64 @@
+import Image from "next/image";
+
+import MailIcon from "@/public/mail.svg";
+
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1 className="font-extrabold">Choeur des Pays du Mont-Blanc</h1>
+    <>
+      <main className="py-8 text-zinc-900">
+        <article className="container mx-auto">
+          <h3 className="mb-4 text-xl font-bold text-sky-700">
+            Le CPMB a le plaisir de vous présenter son nouveau Chef de Chœur !
+          </h3>
+          <div className="flex items-center gap-8">
+            <Image
+              src="/media/benoit_dubu.jpg"
+              alt="Benoît Dubu"
+              width={200}
+              height={350}
+              className="rounded-md object-cover"
+            />
+            <div>
+              <p>Le CPMB, est dirigé par Benoît Dubu, Chef de choeur professionnel depuis le mois de janvier 2024.</p>
+              <p className="pt-1">
+                Le programme de la saison 2024/2025 est un programme &ldquo; autour des <i>Gloria</i> de Vivaldi et de
+                Jenkins &rdquo;.
+              </p>
+              <p className="pt-1">De nombreux concerts sont déjà programmés :</p>
+              <ul className="list-disc pt-1 pl-5">
+                <li>14 juin 2025 : à l&apos;église de Boëge à 20h30.</li>
+                <li>15 juin 2025 : à l&apos;église de Saint Gervais à 18h.</li>
+              </ul>
+              <p className="pt-1">Pour l&apos;occasion, nous serons accompagnés par deux talentueuses solistes :</p>
+              <ul className="list-disc pt-1 pl-5">
+                <li>Helena Duckert : soprano.</li>
+                <li>Chloé Roussel : mezzo-soprano.</li>
+              </ul>
+              <p className="pt-1">Le 12 décembre 2025 : concert de Noël à l&apos;église de Vétraz-Monthoux à 18h.</p>
+              <p className="pt-1">De la très belle musique pour cette nouvelle saison 2024/2025 !</p>
+            </div>
+          </div>
+        </article>
       </main>
-      <footer>Choeur des Pays du Mont-Blanc©2025</footer>
-    </div>
+      <div className="bg-sky-50">
+        <div className="container mx-auto py-10">
+          <h3 className="mb-4 text-xl font-bold text-sky-700">Le CPMB recrute !</h3>
+          <div className="max-w-1/2 rounded-md bg-white p-4 shadow-md">
+            <h4 className="font-noto text-lg font-bold text-gray-800 uppercase">Nous avons besoin de vos voix !</h4>
+            <p className="pt-3">
+              Nous recrutons des choristes ayant une expérience chorale et /ou une capacité en déchiffrage.
+            </p>
+            <p>Celles et ceux qui souhaitent venir nous rejoindre peuvent demander des informations par courriel à</p>
+            <p className="flex items-center gap-2 py-2 pl-4">
+              <MailIcon className="h-5 w-5" />
+              <a href="mailto:bureau@choeurdespaysdumontblanc.fr" className="text-sky-700 hover:underline">
+                bureau@choeurdespaysdumontblanc.fr
+              </a>
+            </p>
+            <p className="pt-3">N&apos;hésitez pas !</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
